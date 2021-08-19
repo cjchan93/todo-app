5b5e3187c7da2ee6a151ea23fb61e812870ab61f
cjchan93 <88640275+cjchan93@users.noreply.github.com>
2021-08-19 11:17:46 +0800

    Add files via upload

---
+++ todo-server/todo.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    id: 1,
+    title: 'Learn GraphQL',
+    done: false
+}, 
+{   id: 2,
+    title: 'Learn ApolloLibrary',
+    done: false
+}, 
+{   id: 3,
+    title: 'Learn Schemas',
+    done: false
+}]
